c72dd2b848c4f1c3104fc6a3784c194dab7b7365
Daniel Fróes <danielfroes15@gmail.com>
2019-03-31 02:07:51 -0300

    Assembler Script

---
+++ Assets/Scripts/Assembler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assembler : MonoBehaviour
+{   
+    [System.Serializable]
+    private class Material
+    {
+        public int quantityNeeded;
+        public int quantityCollected;
+        public string name;
+    }
+
+    [SerializeField] private Material[] materialCheckList;
+    private bool collectedSomething;
+     
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        name  = other.gameObject.tag;
+        foreach (Material material in materialCheckList)
+        {
+            Debug.Log(other.gameObject.tag);   
+            if((material.name == other.gameObject.tag) && (material.quantityNeeded > material.quantityCollected))
+            {
+                material.quantityCollected += 1;
+                collectedSomething = true;  
+            }
+        }
+        if(!collectedSomething)
+        {
+            print("rage++");
+        }
+        collectedSomething = false;
+        GameObject.Destroy(other.gameObject);
+    }
+
+}
