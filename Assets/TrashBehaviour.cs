8472bb717bf6bbfea665c5e4280e75326c7c8cd7
luifr <luifr@usp.br>
2019-03-31 05:37:10 +0000

    trash

---
+++ Assets/TrashBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBehaviour : MonoBehaviour
+{
+    
+    [SerializeField]
+    float scaleFactor;
+
+    void OnTriggerEnter2D(Collider2D other){
+        StartCoroutine(Vanish(other.gameObject));
+    }
+
+    IEnumerator Vanish(GameObject material){
+        for(int i=0;i<10;i++){
+            material.transform.localScale = new Vector3(material.transform.localScale.x*scaleFactor,material.transform.localScale.x*scaleFactor);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+}
